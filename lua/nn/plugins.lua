3e1896007069b771c634b252ee234535765d54f4
Feli Candal <fcandalcampos@gmail.com>
2022-05-25 22:00:47 -0300

    feat(gitsigns): added gitsigns

--- lua/nn/plugins.lua
+++ lua/nn/plugins.lua
@@ -83,6 +83,9 @@ return packer.startup(function(use)
   use "nvim-treesitter/playground"
   use "p00f/nvim-ts-rainbow"
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then

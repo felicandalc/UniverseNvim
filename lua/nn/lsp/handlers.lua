550a54f05f6f415d3add6dcc680021e8e3b962c2
Feli Candal <fcandalcampos@gmail.com>
2022-06-01 17:04:26 -0300

    Revert "fix(lsp): swap server_capabilities with resolved_capabilities"
    This reverts commit 56089c008fe0de37ea6ed1c97635c92286eaea4d.

--- lua/nn/lsp/handlers.lua
+++ lua/nn/lsp/handlers.lua
@@ -92,7 +92,7 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
   end
   if client.name == "gopls" then
     vim.api.nvim_command("au BufWritePost <buffer> lua vim.lsp.buf.format()")

8deeb0ee069b6213be7da84a7a4e00308c04a2cb
Feli Candal <fcandalcampos@gmail.com>
2022-07-04 09:58:55 -0300

    fix(nvim-tree): update moved setup options

--- lua/nn/nvim-tree.lua
+++ lua/nn/nvim-tree.lua
@@ -30,7 +30,7 @@ nvim_tree.setup({
 	open_on_tab = false,
 	hijack_cursor = false,
 	update_cwd = true,
-	update_to_buf_dir = {
+	hijack_directories = {
 		enable = true,
 		auto_open = true,
 	},
@@ -66,7 +66,6 @@ nvim_tree.setup({
 		height = 30,
 		hide_root_folder = false,
 		side = "left",
-		auto_resize = true,
 		mappings = {
 			custom_only = false,
 			list = {
@@ -85,6 +84,7 @@ nvim_tree.setup({
 	actions = {
 		open_file = {
 			quit_on_open = false,
+			resize_window = true,
 			window_picker = {
 				enable = false,
 			},

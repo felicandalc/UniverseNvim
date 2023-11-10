2845fd7865a19e78d6cf5e7f463bb06ec0e3e29c
Felipe Candal Campos <fcandalcampos@gmail.com>
2023-11-10 16:25:47 -0300

    setup `mason-null-ls`

--- lua/nn/lsp/mason.lua
+++ lua/nn/lsp/mason.lua
@@ -1,6 +1,6 @@
 local status_ok, mason = pcall(require, "mason")
 if not status_ok then
-  vim.notify("Mason could not be loaded!", "error")
+	vim.notify("Mason could not be loaded!", "error")
 	return
 end
 
@@ -8,8 +8,16 @@ mason.setup()
 
 local status_ok, mason_lsp_config = pcall(require, "mason-lspconfig")
 if not status_ok then
-  vim.notify("Mason LSPConfig could not be loaded!", "error")
+	vim.notify("Mason LSPConfig could not be loaded!", "error")
 	return
 end
 
 mason_lsp_config.setup()
+
+local status_ok, mason_null_ls = pcall(require, "mason-null-ls")
+if not status_ok then
+	vim.notify("Mason Null LS could not be loaded!", "error")
+	return
+end
+
+mason_null_ls.setup()
